61756fee8cb6924ca8e07531ac828308a57b31a8
khushbu <dancer.khushbu14@gmail.com>
2025-05-02 18:55:57 +0530

    Add environment variables in app.js

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ app.get('/', (req, res) => {
     res.render('index', { portfolioData });
 });
 
-app.listen(3000, () => {
-    console.log('Server running on http://localhost:3000');
+const port=process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
